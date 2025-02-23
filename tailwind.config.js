16c68ddd378891fd2d7a2e415d6c712fdbd0cc01
Carlos Quinzá Pérez <cquinzaperez@gmail.com>
2025-02-23 21:35:23 +0100

    feat: redesign responsive feat: add comanies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}'],
     theme: {
+      screens: {
+        tall: { raw: '(min-height:600px)' },
+      },
       extend: {
       },
     },
